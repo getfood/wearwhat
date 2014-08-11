271b36d89cea6c10c7adde41cef8479d805d84ca
lobatt <lob4tt@gmail.com>
2014-08-11 10:17:22 -0400

    investigate forecast.io api

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,15 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+angular.module('wearWhat', [
+	'ngRoute',
+	'wearWhat.filters',
+	'wearWhat.services',
+	'wearWhat.directives',
+	'wearWhat.controllers'
+	]).
+config(['$routeProvider', '$httpProvider', function($routeProvider,$httpProvider) {
+	$routeProvider.when('/default', {templateUrl: 'partials/default.html', controller: 'Default'});
+	$routeProvider.when('/weather', {templateUrl: 'partials/weather.html', controller: 'Weather'});
+	$routeProvider.otherwise({redirectTo: '/default'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,32 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+angular.module('wearWhat.controllers', [])
+.controller('Default', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+}])
+.controller('Weather', ['$scope', '$routeParams', '$http', function($scope,  $routeParams, $http) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position){
+			$scope.$apply(function(){
+				console.log(position);
+				$scope.apiKey =  $routeParams.apiKey;
+				$scope.position = position;
+				$scope.latitude = position.coords.latitude;
+				$scope.longitude = position.coords.longitude;
 
-  }]);
+				if ($routeParams.apiKey && position) {
+					var query = "?units=si&exclude=minutely,alerts,daily,hourly,flags&callback=JSON_CALLBACK";
+					var forecastIoUri = "https://api.forecast.io/forecast/"+ $routeParams.apiKey+"/" + position.coords.latitude + "," + position.coords.longitude;
+					$http.jsonp(forecastIoUri+query)
+					.success(function(status, response){console.log(status); $scope.weather = status;})
+					.error(function(status, response){$scope.weather = response;});
+				} else {
+					$scope.weather = "Unknown";
+				}
+			});
+		});
+		
+		
+	}
+}]);
